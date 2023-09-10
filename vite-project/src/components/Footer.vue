3093cfe9660960022556cacbac5f7faaf728e66e
Zehra Betül Demir <120724327+zbetuldemirrr@users.noreply.github.com>
2023-09-10 16:42:59 +0300

    Footer.vue

--- vite-project/src/components/Footer.vue
+++ vite-project/src/components/Footer.vue
@@ -1,23 +1,21 @@
 <template>
-  <footer class="footer">
-
-      <h1 class="footer-title"> Footer</h1>
-  </footer>
+  <div class="footer">
+   <h2>{{ title }}</h2>
+  </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+  import { defineProps } from "vue";
 
-const props = defineProps({
-  footer: String,
-});
-
-console.log(props.footer); 
+  const props = defineProps({
+    title: String,
+  });
+  console.log( props.title);
 </script>
 
 <style scoped>
 .footer {
-   background-color: #ccc;
+  background-color: #ccc;
   border: 1px solid #000; 
   color: white;
   text-align: center;
@@ -36,10 +34,11 @@ bottom:0px;
 
 }
 
- .footer-title{
-  font-size:170px; 
-  text-align: center; 
 
+ .footer h2 {
+  font-size: 150px;
+  text-align:center;
+  color: #000;
  
  }
 </style>
